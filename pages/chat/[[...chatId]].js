632766b5166a1a8f6d0a676574c4343f0c61c974
Jose Rubio <jadrubio@gmail.com>
2023-08-11 11:44:36 -0400

    sign up functionality implemented

---
+++ pages/chat/[[...chatId]].js
@@ -0,0 +1,9 @@
+import Head from "next/head";
+
+export default function ChatId() {
+    return (
+        <div>
+            <p>chat id page</p>
+        </div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,43 @@
 import Head from "next/head";
 import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Home() {
+  const { isLoading, error, user } = useUser();
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
   return (
-    <div>
+    <>
       <Head>
-        <title>Next JS ChatGPT Starter</title>
+        <title>Fizzingbuns - The Sagely Gnome</title>
       </Head>
-      <h1>Welcome to the Next JS &amp; ChatGPT Starter</h1>
+      <div className="flex min-h-screen w-full items-center justify-center bg-gray-800 text-center text-white">
         <div>
-            <Link href="/api/auth/login">Login</Link>
+          {user ? (
+            <Link
+              href="/api/auth/logout"
+              className="rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600"
+            >
+              Logout
+            </Link>
+          ) : (
+            <>
+              <Link
+                href="/api/auth/login"
+                className="rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600"
+              >
+                Login
+              </Link>
+              <Link
+                href="/api/auth/signup"
+                className="rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600"
+              >
+                Sign Up
+              </Link>
+            </>
+          )}
         </div>
-    </div>
+      </div>
+    </>
   );
 }
