2ab02e95cb5689859f528f52fea73a9d4b8ceb11
Jose Rubio <jadrubio@gmail.com>
2023-08-13 13:43:16 -0400

    basic chat side bar with basic stylings for chat page

--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -1,11 +1,12 @@
 import Head from "next/head";
+import {ChatSideBar} from "../../components/ChatSideBar";
 
 export default function ChatId() {
     return (
       <>
       <Head>Start A Chat</Head>
       <div className="grid h-screen grid-cols-[260px_1fr]">
-        <div>sidebar</div>
+        <ChatSideBar />
         <div className="bg-gray-700 flex flex-col">
           <div className="flex-1"> chat window</div>
           <footer className="bg-gray-800 p-10">footer</footer>
