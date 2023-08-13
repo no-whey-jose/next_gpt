c3b00d6903b302c2e970e1319f904403a2082869
Jose Rubio <jadrubio@gmail.com>
2023-08-13 15:36:19 -0400

    capturing user input and submit handler set up

--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -1,7 +1,13 @@
 import Head from "next/head";
 import { ChatSideBar } from "../../components/ChatSideBar";
+import {useState} from "react";
 
 export default function ChatPage() {
+  const [userInput, setUserInput] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('userInput', userInput)
+  }
   return (
     <>
       <Head>Start A Chat</Head>
@@ -10,11 +16,13 @@ export default function ChatPage() {
         <div className="flex flex-col bg-gray-700">
           <div className="flex-1"> chat window</div>
           <footer className="bg-gray-800 p-10">
-            <form>
+            <form onSubmit={handleSubmit}>
               <fieldset className="flex gap-2">
                 <textarea
                   className="w-full resize-none rounded-md bg-gray-700 p-2 text-white focus:border-red-500 focus:bg-gray-600 focus:outline focus:outline-red-500"
                   placeholder="Strike up a conversation..."
+                  value={userInput}
+                  onChange={(e) => setUserInput((e.target.value))}
                 />
                 <button type="submit" className="btn">
                   Send
