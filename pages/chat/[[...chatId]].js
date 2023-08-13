42372d6bf81d20918d2cbf29f5d82a82be4c565e
Jose Rubio <jadrubio@gmail.com>
2023-08-13 13:33:38 -0400

    basic layout with basic stylings for chat page

--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -2,8 +2,16 @@ import Head from "next/head";
 
 export default function ChatId() {
     return (
-        <div>
-            <p>chat id page</p>
+      <>
+      <Head>Start A Chat</Head>
+      <div className="grid h-screen grid-cols-[260px_1fr]">
+        <div>sidebar</div>
+        <div className="bg-gray-700 flex flex-col">
+          <div className="flex-1"> chat window</div>
+          <footer className="bg-gray-800 p-10">footer</footer>
         </div>
+      </div>
+      </>
+
     );
 }
